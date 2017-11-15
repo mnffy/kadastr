32486bb7d378a7f85104c6b0cb82528236ead483
Maxim Shvatov <masaks2010@yandex.ru>
2017-11-15 19:06:22 +0300

    C4 - initial project commit with base auth

---
+++ Project/Cadastral/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cadastral.Startup))]
+namespace Cadastral
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
